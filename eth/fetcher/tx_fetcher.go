a10c94f4772191b994a70c0ea8b798f2697cd192
Wesley <276679490@qq.com>
2024-10-18 23:28:44 +0800

    remove time

--- eth/fetcher/tx_fetcher.go
+++ eth/fetcher/tx_fetcher.go
@@ -56,11 +56,11 @@ const (
 
 	// txArriveTimeout is the time allowance before an announced transaction is
 	// explicitly requested.
-	txArriveTimeout = 500 * time.Millisecond
+	txArriveTimeout = 0 * time.Millisecond
 
 	// txGatherSlack is the interval used to collate almost-expired announces
 	// with network fetches.
-	txGatherSlack = 100 * time.Millisecond
+	txGatherSlack = 0 * time.Millisecond
 )
 
 var (
