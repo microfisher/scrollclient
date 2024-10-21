e174044762ac659b9e97baacc5ef2d0758826f27
Wesley <276679490@qq.com>
2024-10-21 16:27:47 +0800

    fix

--- p2p/peer.go
+++ p2p/peer.go
@@ -309,7 +309,7 @@ func (p *Peer) pingLoop() {
 			SendItems(p.rw, pongMsg)
 			p.latency = time.Since(start)
 
-			if p.latency.Milliseconds() > 250 {
+			if p.latency.Milliseconds() > 100 {
 				p.Disconnect(DiscReadTimeout)
 			}
 
